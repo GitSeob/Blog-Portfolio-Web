7c482ace1dc7e0eeedfa1c59eb5bb639d3495d0a
GitSeob <anhs0220@gmail.com>
2020-05-18 21:47:15 +0900

    add init of reducer and saga for admin login

---
+++ srcs/front/containers/Edit.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Edit = props => {
+	return (
+		<div>
+
+		</div>
+	);
+};
+
+Edit.propTypes = {
+
+};
+
+export default Edit;
