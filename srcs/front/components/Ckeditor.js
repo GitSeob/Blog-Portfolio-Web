cbd1c3aa20f2ff55d5446d54ec7b7d5d5089d112
GitSeob <anhs0220@gmail.com>
2020-06-01 20:19:03 +0900

    ckeditor is displayed but value error

--- srcs/front/components/Ckeditor.js
+++ srcs/front/components/Ckeditor.js
@@ -1,22 +1,36 @@
 import React from 'react';
 import CKEditor from "@ckeditor/ckeditor5-react";
-import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
-// import ClassicEditor from "@ckeditor/ckeditor5-build-classic"
-
-import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
-import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
-import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
-
-import PropTypes from 'prop-types';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { useInput } from '../pages/login';
 
 const Ckeditor = props => {
+	const [ckval, OCV] = useInput('');
 	return (
 		<div className="App">
-			<h1> here is editor</h1>
-			<CKEditor
-				editor={ClassicEditor}
-			/>
+			<div className="container">
+				<div className="wrapper">
+					<form className="form-group">
+						<div className="form-group">
+							<label>Name</label>
+							<input type="text" name="name" className="form-control"/>
+						</div>
+						<div className="form-group">
+							<label>email</label>
+							<input type="text" name="email" className="form-control"/>
+						</div>
+						<div className="form-group">
+							<CKEditor
+								editor={ClassicEditor}
+								onInit={ editor=>{
+									console.log(editor);
+								}}
+								value={ckval}
+								onChange={OCV}
+							/>
+						</div>
+					</form>
+				</div>
+			</div>
 		</div>
 	);
 };
