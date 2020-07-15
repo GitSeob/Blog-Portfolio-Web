3503353c48353a529770acea543639e6fb49e7b3
Hongseob An <han@c2r2s5.42seoul.kr>
2020-07-15 20:09:38 +0900

    remove HYDRATE, action console command

--- srcs/front/reducers/index.js
+++ srcs/front/reducers/index.js
@@ -9,7 +9,6 @@ import information from './information';
 const rootReducer = (state, action) => {
 	switch (action.type) {
 		case HYDRATE:
-			console.log('HYDRATE, action');
 			return action.payload;
 		default: {
 			const combineReducer = combineReducers({
