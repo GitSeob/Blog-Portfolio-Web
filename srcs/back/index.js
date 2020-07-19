fb5b7395f45ab171feec0ef257ba5d48d5dd7d3b
GitSeob <anhs0220@gmail.com>
2020-07-19 17:35:54 +0900

    editing for api.domain

--- srcs/back/index.js
+++ srcs/back/index.js
@@ -20,12 +20,6 @@ const informationAPIRouter = require('./routes/information');
 dotenv.config();
 const app = express();
 
-//require('greenlock-express').init({
-//	packageRoot: __dirname,
-//	configDir: './greenlock.d',
-//	maintainerEmail: 'anhs0220@gmail.com'
-//}).server(app);
-
 db.sequelize.sync()
 	.then(() => {
 		console.log('✓ DB connection success.');
@@ -38,56 +32,37 @@ db.sequelize.sync()
 });
 passportConfig();
 
-//if (process.env.NODE_ENV === 'production') {
-//		app.use(morgan('combined'));
-//		app.use(hpp());
-//		app.use(helmet());
-//		app.use(cors({
-//		origin: /* url */,
-//		credentials: true,
-//		}));
-//	} else {
-//		app.use(morgan('dev'));
-//		app.use(cors({
-//		origin: true,
-//		credentials: true,
-//	}));
-//}
-
-app.use(morgan('dev')) // 요청들어오면 log가 남음
+app.use(morgan('dev'))
 app.use('/globalImg', express.static('globalImg'));
 app.use('/img_for_portfolio', express.static('img_for_portfolio'));
-app.use('/', express.static('uploads')) // uploads 폴더를 다른 서버에서 자유롭게 가져갈 수 있게 해준다
+app.use('/', express.static('uploads'))
 app.use(cors({
-	origin: true,
-	credentials: true, // 서버쪽에서도 쿠키 주고받을 수 있게
-})) // 다른 서버에서 요청을 받을 수 있게 허용하게 해준다. 추가안하면 서버에서 거절함
+	origin: 'http://anjoy.info',
+	credentials: true,
+}))
 app.use(express.json({
 	limit: "50mb",
-})) // json 사용하기 위해
+}))
 app.use(express.urlencoded({
 	extended: true,
 	limit: "50mb",
 	parameterLimit: 1000000,
-})) // form형식으로 전달하기 위해
+}))
 app.use(cookieParser(process.env.COOKIE_SECRET))
 app.use(expressSession({
-	resave: false, // 매번 새션 강제 저장
-	saveUninitialized: false, // 빈 값도 저장
-	secret: process.env.COOKIE_SECRET, // 쿠키 보안코드
+	resave: false,
+	saveUninitialized: false,
+	secret: process.env.COOKIE_SECRET,
 	cookie: {
-		httpOnly: true, // 자바스크립트에서 쿠키에 접근 불가 옵션
-		secure: false, // https를 쓸 때 true로 해주면 된다.
-		//domain: process.env.NODE_ENV === 'production' && '.nodebird.com'
+		httpOnly: true,
+		secure: false,
+		domain: process.env.NODE_ENV === 'production' && '.anjoy.info'
 	},
-	name: 'anjoy_blog' // cookie이름을 바꿔야함 conect.sid면 express를 쓰는 걸 들킨다
-}))
-// 쿠키랑 세션 미들웨어 붙임
+	name: 'anjoy_blog'
+}));
 
 app.use(passport.initialize());
 app.use(passport.session());
-// passport 세션은 express 세션 뒤에 붙여야 한다. 반드시.
-// 미들웨어간 의존 관계가 있기 때문에 의존 관계가 있는 경우에는 순서가 매우 중요하다.
 
 app.use('/api/user', userAPIRouter);
 app.use('/api/post', postAPIRouter);
@@ -97,6 +72,6 @@ app.use('/api/category', categoryAPIRouter);
 app.use('/api/information', informationAPIRouter);
 app.use('/api/portfolio', portAPIRouter);
 
-app.listen(3065, () => {
-	console.log('server is running on localhost:3065 !');
+app.listen(80, () => {
+	console.log('server is running !');
 });
--- srcs/back/routes/user.js
+++ srcs/back/routes/user.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const db = require('../models');
-const bcrypt = require('bcrypt');
 const passport = require('passport');
 
 const { isLoggedIn } = require('./middleware');
@@ -32,10 +31,6 @@ router.post('/login', (req, res, next) => {
 					attributes: ['id', 'nickname', 'userId']
 				})
 				return res.json(fullUser);
-				// const filteredUser = Object.assign({}, user.toJSON())
-                // delete filteredUser.password // 프론트에 비밀번호를 전달하면 위험하기 때문에 delete를 이용하여 user 객체 안의 password를 지워주고 보내준다.
-                // console.log(filteredUser)
-                // return res.json(filteredUser)
 			} catch(e) {
 				next(e);
 			}
